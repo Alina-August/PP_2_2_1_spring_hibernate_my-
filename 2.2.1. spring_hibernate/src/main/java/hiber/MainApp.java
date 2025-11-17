3f52ea154137a3de16d0cf4ee9cd874e6c98d48a
Alina-August <alina-august@mail.ru>
2025-11-17 20:19:23 +0500

    Задача решена

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -4,6 +4,7 @@ import hiber.config.AppConfig;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import hiber.model.Car;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -15,10 +16,15 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+       Car car1 = new Car("BMW", 5);
+       Car car2 = new Car("Audi", 7);
+       Car car3 = new Car("Mercedes", 63);
+       Car car4 = new Car("Porsche", 911);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
